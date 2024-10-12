e890c3b7baa0c255ac0e1364fc04333f6a005097
JM Hore <kira@MacBook-Air.local>
2024-10-12 12:37:18 -0700

    added search icon in search page

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import searchIcon from "../assets/searchIcon.svg";
 
 function SearchBox (props) {
 
     return (
         <div className="search-bar">
-            <input  placeholder="Type To Search" 
+            <input  placeholder="Type to search"
                     value={props.value} 
                     onChange={() => props.setSearchValue(event.target.value)} 
             />
+            <img src={searchIcon} alt="Search Icon" className="search-icon" />
         </div>
     )
 }
