a1b1975159fc0fb84c2d0cd06a2000762c742e77
JM Hore <kira@MacBook-Air.local>
2024-10-12 14:44:47 -0700

    updated page not found

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -3,13 +3,16 @@ import Footer from '../components/Footer';
 import Nav from '../components/Nav';
 
 function PageNotFound() {
-    return
+    return (
     <>
     
     <Nav/>
+    <h1 style={{textAlign: 'center'}}>404 Error.. Page Not Found!!!</h1>
     <Footer/>
 
     </>
+
+    )
 }
 
 export default PageNotFound;
